60bdf33a3cdd029d30a3f2675e3fdb024de3b1c2
Lohan <ltwm1998@gmail.com>
2023-11-12 16:46:30 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    first = 0
+    second = 1
+
+    if length == 1:
+        print([0])
+
+    elif length > 1:
+        fib_sequence = [first, second]
+        length -= 2
+
+        while length > 0:
+
+            next_number = first + second
+            fib_sequence.append(next_number)
+
+            temp = second
+            second = first + second
+            first = temp
+
+            length -= 1
+
+        print(fib_sequence)
+    else:
+        print([])
